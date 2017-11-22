cd169bedd869d742588872cf252ffb4b2d3c8e49
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-11-22 18:02:28 +0100

    added updateRoles method

--- src/main/webapp/user/userController.js
+++ src/main/webapp/user/userController.js
@@ -2,14 +2,31 @@
     angular.module("synxApp")
         .controller("userController", ["$scope", "userService", function($scope, userService) {
 
-            userService.getAllUsers()
-                .then(function(response) {
-                    $scope.users = response.data;
-                    console.log($scope.users);
-                }, 
-                function(error) {
-                    console.log("Can't load users");
-                });
+            function getAll() {
+                userService.getAllUsers()
+                    .then(function(response) {
+                        $scope.users = response.data;
+                        console.log($scope.users);
+                    }, 
+                    function(error) {
+                        console.log("Can't load users");
+                    });
+            }
+            getAll();
 
+            $scope.updateRoles = function() {
+                $scope.users.forEach(user => {
+                    if (user.newRole === "ADMIN" || user.newRole === "USER" || user.newRole === "MODERATOR") {
+                        console.log(user.newRole);
+                        userService.updateRoles(user.username, user.newRole)
+                            .then(function(response) {
+                                console.log("Role successfully changed" + response.data);
+                            }, function(error) {
+                                console.log("Unable to update role" + error.data);
+                            });
+                    }
+                });
+                getAll();
+            }
         }]);
 })();
