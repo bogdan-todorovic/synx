d409ad7db94e761196a10f793761e8d8df76dde8
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-11-26 21:17:05 +0100

    getSubforum method

--- src/main/webapp/subforum/subforumService.js
+++ src/main/webapp/subforum/subforumService.js
@@ -1,12 +1,16 @@
 (function() {
     angular.module("synxApp")
-        .service("subforumSvc", ["$http", function($http) {
+        .service("subforumService", ["$http", function($http) {
 
             var self = this;
             self.baseUrl = "rest/subforums";
 
             self.getAllSubforums = function() {
                 return $http.get(self.baseUrl);
+            };
+
+            self.getSubforum = function(id) {
+                return $http.get(self.baseUrl + "/" + id);
             };
 
             self.createSubforum = function(data) {
