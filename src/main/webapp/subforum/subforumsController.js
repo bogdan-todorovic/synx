2a23f6ad6f0336bb11feb8417a25293e5159bdd1
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-11-26 21:19:11 +0100

    reworking of the controller started

--- src/main/webapp/subforum/subforumsController.js
+++ src/main/webapp/subforum/subforumsController.js
@@ -1,13 +1,9 @@
 (function() {
     angular.module("synxApp")
-        .controller("subforumsController", ["$scope", "subforumSvc", function($scope, subforumSvc) {
+        .controller("subforumsController", ["$scope", "$rootScope", "subforumService",
+            function($scope, $rootScope, subforumService) {
 
-            $scope.subforums = {};
-            $scope.subforum = {};
-            $scope.createOrEdit = true;
-
-            function getAll() {
-                subforumSvc.getAllSubforums()
+                subforumService.getAllSubforums()
                     .then(function(response) {
                             $scope.subforums = response.data;
                         },
@@ -16,66 +12,5 @@
                         }
                     );
             }
-            getAll();
-
-            $scope.submit = function() {
-                if ($scope.createOrEdit) {
-                    create();
-                } 
-                else {
-                    console.log("APDEJTUJ POSTOJECI");
-                    update();
-                } 
-
-            };
-
-            var create = function() {
-                subforumSvc.createSubforum($scope.subforum)
-                    .then(function(response) {
-                        $scope.subforums.push(response.data);
-                        $scope.successMessage = "Subforum successfully created";
-                    }, 
-                    function(error) {
-                        $scope.errorMessage = "Subforum with that title already exists";
-                    });
-            };
-
-            var update = function() {
-                subforumSvc.updateSubforum($scope.subforum)
-                    .then(function(response) {
-                        getAll();
-                        $scope.successMessage = "Subforum successfully updated";
-                    },
-                    function(error) {
-                        $scope.errorMessage = "Subforum with that title already exists";
-                    }
-                    );
-            };
-
-            $scope.delete = function(sf) {
-                console.log(sf);
-                subforumSvc.removeSubforum(sf.title)
-                    .then(function(response) {
-                        var index = $scope.subforums.indexOf(sf);
-                        if (index !== -1) {
-                            $scope.subforums.splice(index, 1); 
-                        } 
-                    },
-                    function(error) {
-                        console.log("unsuccessfully");
-                    }
-                    );
-            };
-
-            $scope.edit = function(sf) {
-                //cloninig object
-                $scope.subforum = JSON.parse(JSON.stringify(sf));
-                $scope.createOrEdit = false;
-            };
-
-            $scope.resetForm = function() {
-                $scope.subforum = {};
-                $scope.createOrEdit = true;
-            };
-        }]);
+        ]);
 })();
