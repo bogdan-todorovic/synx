49f76317d856dbcae5b0cbf15ba4c9c94322f3bc
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-12-04 19:03:20 +0100

    preview image before uploading functionality

--- src/main/webapp/subforum/subforumsController.js
+++ src/main/webapp/subforum/subforumsController.js
@@ -1,8 +1,9 @@
 (function() {
     angular.module("synxApp")
-        .controller("subforumsController", ["$scope", "$rootScope", "subforumService",
-            function($scope, $rootScope, subforumService) {
+        .controller("subforumsController", ["$scope", "$rootScope", "fileReaderService", "subforumService",
+            function($scope, $rootScope, fileReaderService, subforumService) {
 
+                // Load all subforums
                 subforumService.getAllSubforums()
                     .then(function(response) {
                             $scope.subforums = response.data;
@@ -11,6 +12,21 @@
                             $scope.alertMessage = "Unable to load subforums";
                         }
                     );
+                
+                 // instance for new subforum
+                $scope.newSf = {};
+                
+                // load and preview selected image
+                $scope.loadFile = function () {
+                    fileReaderService.readAsDataUrl($scope.chosenFile, $scope)
+                        .then(function(result) {
+                            $scope.newSf.icon = result;
+                        });
+                };
+                
+                $scope.create = function() {
+                    
+                }
             }
         ]);
 })();
