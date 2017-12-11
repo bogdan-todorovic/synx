ff396a5286297674aa6aa3c11909c356409a2621
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-12-11 20:04:29 +0100

    dynamically adding form fields for new rules

--- src/main/webapp/subforum/subforumsController.js
+++ src/main/webapp/subforum/subforumsController.js
@@ -9,23 +9,44 @@
                             $scope.subforums = response.data;
                         },
                         function() {
-                            $scope.alertMessage = "Unable to load subforums";
+                            console.log("Unable to load subforums");
                         }
                     );
-                
+                 
                  // instance for new subforum
                 $scope.newSf = {};
                 
-                // load and preview selected image
+
+                // the file as a URL 
+                // representing the file's data as a base64 encoded string
                 $scope.loadFile = function () {
                     fileReaderService.readAsDataUrl($scope.chosenFile, $scope)
                         .then(function(result) {
                             $scope.newSf.icon = result;
                         });
                 };
-                
+
+                // method which adds new text input field on button click
+                // and adds the rule into the rules list
+                $scope.newSf.rules = [];
+                $scope.addNewRule = function() {
+                    $scope.newSf.rules.push("");
+                    console.log($scope.newSf.rules);
+                }
+
+                // method which removes previously added rule from the rules list
+                $scope.removeRule = function(index) {
+                    $scope.newSf.rules.splice(index, 1);
+                }
+
                 $scope.create = function() {
-                    
+                    subforumService.createSubforum($scope.newSf)
+                        .then(function(response) {
+                            console.log("succesfully created");
+                        }, function(error) {
+                            $scope.alertMessage = "Subforum with that title already exists";
+                            console.log(error.data);
+                        });
                 }
             }
         ]);
