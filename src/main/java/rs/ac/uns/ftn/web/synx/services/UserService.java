d10b58ddb7552299a6ab5f817a7119f4c985e36a
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-12-20 17:55:42 +0100

    rest endpoint for getting all moderators

--- src/main/java/rs/ac/uns/ftn/web/synx/services/UserService.java
+++ src/main/java/rs/ac/uns/ftn/web/synx/services/UserService.java
@@ -1,11 +1,16 @@
 package rs.ac.uns.ftn.web.synx.services;
 
+import java.util.List;
+
 import rs.ac.uns.ftn.web.synx.model.User;
 
 public interface UserService extends CrudService<User, String> {
 
 	User authenticate(String username, String password);
-	/* Returns true if role is successfully changed */
+	
+	/* Returns true if role is successfully changed otherwise false */
 	boolean changeRole(User user, String newRole);
+	
+	List<User> findAllModerators();
 
 }
