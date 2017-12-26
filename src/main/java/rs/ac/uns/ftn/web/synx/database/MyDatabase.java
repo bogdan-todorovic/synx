eeeccf8609ead8d791adea834d79a5dbc8517363
bogdan-todorovic <bogdantodorovic93@gmail.com>
2017-12-26 21:41:36 +0100

    added topics collection

--- src/main/java/rs/ac/uns/ftn/web/synx/database/MyDatabase.java
+++ src/main/java/rs/ac/uns/ftn/web/synx/database/MyDatabase.java
@@ -3,6 +3,7 @@ package rs.ac.uns.ftn.web.synx.database;
 import java.util.Map;
 
 import rs.ac.uns.ftn.web.synx.model.Subforum;
+import rs.ac.uns.ftn.web.synx.model.Topic;
 import rs.ac.uns.ftn.web.synx.model.User;
 import rs.ac.uns.ftn.web.synx.util.Serializer;
 
@@ -10,6 +11,7 @@ public class MyDatabase {
 	
 	private static Map<String, User> users = Serializer.load("users.ser");
 	private static Map<String, Subforum> subforums = Serializer.load("subforums.ser");
+	private static Map<String, Topic> topics = Serializer.load("topics.ser");
 	
 	public static Map<String, User> getUsers() {
 		return users;
@@ -27,6 +29,11 @@ public class MyDatabase {
 		MyDatabase.subforums = subforums;
 	}
 	
-	
-	
+	public static Map<String, Topic> getTopics() {
+		return topics;
+	}
+
+	public static void setTopics(Map<String, Topic> topics) {
+		MyDatabase.topics = topics;
+	}
 }
