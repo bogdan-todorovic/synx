25930d9a57e0f178e08a39c1228bc9e581b02024
bogdan-todorovic <bogdantodorovic93@gmail.com>
2018-01-25 19:00:17 +0100

    create root comment operation

--- src/main/webapp/topic/topicController.js
+++ src/main/webapp/topic/topicController.js
@@ -1,11 +1,23 @@
 (function() {
     angular.module("synxApp")
-        .controller("topicController", ["$scope", "topicPromise", "commentsPromise", function($scope, topicPromise, commentsPromise) {
+        .controller("topicController", ["$scope", "topicPromise", "commentsPromise", "commentService", function($scope, topicPromise, commentsPromise, commentService) {
 
             $scope.topic = topicPromise.data;
             console.log($scope.topic);
 
             $scope.comments = commentsPromise.data;
             console.log($scope.comments);
+
+            $scope.newRootComment = {};
+            $scope.newRootComment.topic = $scope.topic.title;
+
+            $scope.createRootComment = function() {
+                commentService.createComment($scope.newRootComment)
+                    .then(function(response) {
+                        $scope.comments.push(response.data);
+                    }, function (error) {
+                        console.log(error);
+                    });
+            };
         }]);
 })();
